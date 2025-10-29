14b61693187fa6b3cd31d4b6b7c8d2f1a3518c77
simukaupenye <simukaupenyeagri@gmail.com>
2025-10-29 18:09:32 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Simuka Upenye Delivery Note System")
+root.geometry("400x200")
+
+label = tk.Label(root, text="Welcome to Simuka Upenye Delivery Note System!", font=("Arial", 12))
+label.pack(pady=50)
+
+root.mainloop()
